067152e1ff500ce516766b14519dc1ef06a953ad
Parksohui <psh990525@naver.com>
2021-05-19 17:31:05 +0900

    feat: transfer page
    Transfer page from Start to Main

--- prupru/app/src/main/java/org/techtown/prupru/MainActivity.kt
+++ prupru/app/src/main/java/org/techtown/prupru/MainActivity.kt
@@ -1,11 +1,14 @@
 package org.techtown.prupru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
--- prupru/app/src/main/java/org/techtown/prupru/StartActivity.kt
+++ prupru/app/src/main/java/org/techtown/prupru/StartActivity.kt
@@ -1,11 +1,20 @@
 package org.techtown.prupru
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val intent= Intent(this, MainActivity::class.java)
+
+        var SignButton= this.findViewById<Button>(R.id.SignButton)
+        SignButton.setOnClickListener {
+            startActivity(intent)
+        }
     }
 }
