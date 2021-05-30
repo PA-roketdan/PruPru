35dce6abccd17d0d15a7590ad08f756942b4e994
Parksohui <psh990525@naver.com>
2021-05-31 03:53:16 +0900

    ui: change ui & name
    Change ui & name
    -Result
    -change ui

--- prupru/app/src/main/java/cnu/rocket/prupru/ResultActivity3.kt
+++ prupru/app/src/main/java/cnu/rocket/prupru/ResultActivity3.kt
@@ -5,10 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 
-class ReasultActivity3 : AppCompatActivity() {
+class ResultActivity3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reasult3)
+        setContentView(R.layout.activity_result3)
 
         var Img_home= this.findViewById<ImageButton>(R.id.Img_home)
         Img_home.setOnClickListener {
